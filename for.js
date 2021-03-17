b1d4f489ad92ada2895cba126aea767ff6a43930
gored-boop <79080461+gored-boop@users.noreply.github.com>
2021-03-17 17:21:42 +0900

    Add files via upload

--- for.js
+++ for.js
@@ -1,9 +1,9 @@
-//var array = [1,2,3,4,5,6,7,8,9,10];
+var array = [1,2,3,4,5,6,7,8,9,10];
 
-//for(var i =0; i<array.length;i++){
-  //  console.log(array[i]);
-//}
-
-for(var i = 0, j = 10; i<j;i++,j--){
-    console.log(i,j);
+for(var i =0; i<array.length;i++){
+    console.log(array[i]);
 }
+
+//for(var i = 0, j = 10; i<j;i++,j--){
+  //  console.log(i,j);
+//}
